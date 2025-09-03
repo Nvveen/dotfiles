f80cf428477adaeba50a84e7febac271b6f46047
Neal van Veen <nealvanveen@gmail.com>
2025-09-03 16:57:51 +0200

    update install for omarchy

--- install.sh
+++ install.sh
@@ -4,11 +4,35 @@
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $CWD
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-ln -svf $CWD/.zshrc $HOME/.zshrc
-
 git submodule update --init --recursive
 
-# link plugins
-ln -svf $CWD/modules/.oh-my-zsh/custom/plugins/* $HOME/.oh-my-zsh/custom/plugins/
+# Determine environment
+
+# Omarchy
+if [[ -d $HOME/.config/omarchy ]]; then
+    ENV="omarchy"
+fi
+
+# Other environments can be added here
+
+setup_omarchy() {
+    sudo yay -S --noconfirm zsh
+
+    # oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    ln -svf $CWD/.zshrc $HOME/.zshrc
+
+    # link plugins
+    ln -svf $CWD/modules/.oh-my-zsh/custom/plugins/* $HOME/.oh-my-zsh/custom/plugins/
+}
+
+# switch on env
+case $ENV in
+omarchy)
+    setup_omarchy
+    ;;
+*)
+    echo "Unknown environment: $ENV"
+    exit 1
+    ;;
+esac
