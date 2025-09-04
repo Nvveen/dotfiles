6638ecbb857a97e63b1462632df126c987f67bce
Neal van Veen <nealvanveen@gmail.com>
2025-09-04 12:17:26 +0200

    fix ssh key name

--- from_git.sh
+++ from_git.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # if authentication fails, there is no key in github
-# if id_rsa_github exists, wait for the user to add it to github
+# if id_rsa exists, wait for the user to add it to github
 # if it doesn't exist, create it and wait for the user to add it to github
 # silence any response from github when checking status
+
+KEY_FILENAME=id_rsa
 
 main() {
   ssh -T git@github.com >/dev/null 2>&1
@@ -12,24 +14,24 @@ main() {
   if [[ $ssh_exit_code == 1 ]]; then
     echo "GitHub authentication successful"
   else
-    if [[ -f "$HOME/.ssh/id_rsa_github" ]]; then
+    if [[ -f "$HOME/.ssh/id_rsa" ]]; then
       echo "Please add your SSH key to GitHub:"
       echo ""
-      cat $HOME/.ssh/id_rsa_github.pub
+      cat $HOME/.ssh/id_rsa.pub
       echo ""
       read -p "Press [Enter] after adding the key to GitHub" </dev/tty
     else
       echo "Creating a new SSH key for GitHub..."
-      ssh-keygen -t rsa -b 4096 -C "nealvanveen@gmail.com" -f $HOME/.ssh/id_rsa_github -N ""
+      ssh-keygen -t rsa -b 4096 -C "nealvanveen@gmail.com" -f $HOME/.ssh/id_rsa -N ""
       echo "Please add your SSH key to GitHub:"
       echo ""
-      cat $HOME/.ssh/id_rsa_github.pub
+      cat $HOME/.ssh/id_rsa.pub
       echo ""
       read -p "Press [Enter] after adding the key to GitHub" </dev/tty
     fi
 
     eval "$(ssh-agent -s)"
-    ssh-add $HOME/.ssh/id_rsa_github
+    ssh-add $HOME/.ssh/id_rsa
     echo "SSH key added to ssh-agent"
 
     ssh -T git@github.com >/dev/null 2>&1
