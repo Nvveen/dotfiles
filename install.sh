129554aa0e1d91555840995d62edf808605758ea
Neal van Veen <nealvanveen@gmail.com>
2025-09-25 16:58:10 +0200

    add stow to install script

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# TODO hyprland bindings
-# TODO alacritty config
-
 # get directory of current script
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $CWD
+DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+STOW_BACKUP="$DOTFILES/.stow_backup"
+CONFIGS="$DOTFILES/configs"
 
-git submodule update --init --recursive
+ONLY_CONFIGS=false
+if [[ " $* " == *" --only-configs "* ]]; then
+  ONLY_CONFIGS=true
+fi
 
 # Determine environment
 
@@ -20,7 +21,7 @@ fi
 
 setup_omarchy() {
   # install packages
-  yay -S --noconfirm zsh firefox stow
+  yay -S --noconfirm zsh firefox stow rsync
   chsh -s /usr/bin/zsh
 
   # Remove packages
@@ -32,20 +33,62 @@ setup_omarchy() {
 
   # oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-  # mirror repo
-
-  # theming
-  omarchy-theme-set osaka-jade
 }
 
-# switch on env
-case $ENV in
-omarchy)
-  setup_omarchy
-  ;;
-*)
-  echo "Unknown environment: $ENV"
-  exit 1
-  ;;
-esac
+setup_os() {
+  git submodule update --init --recursive
+  # switch on env
+  case $ENV in
+  omarchy)
+    setup_omarchy
+    ;;
+  *)
+    echo "Unknown environment: $ENV"
+    exit 1
+    ;;
+  esac
+}
+
+setup_config() {
+  # theming
+  omarchy-theme-set osaka-jade >/dev/null 2>&1
+
+  # mirror repo
+  local TIMESTAMP=$(date +"%Y-%m-%dT%H:%M:%S")
+  local STOW_BACKUP="$STOW_BACKUP/$TIMESTAMP"
+  local STOW_PKG=${CONFIGS//$DOTFILES\//}
+  local EXISTING=$(cd $CONFIGS && find . -type f | cut -c3-)
+
+  stow --verbose --target=$HOME --delete $STOW_PKG
+  # backup original files first
+  for f in $EXISTING; do
+    local ORIGINAL="$HOME/$f"
+    local TARGET="$CONFIGS/$f"
+    local BACKUP_TARGET="$STOW_BACKUP/$f"
+
+    # stow --delete $CONFIGS
+    if [[ -L "$ORIGINAL" ]]; then
+      # remove any existing links
+      echo "Resetting link to $TARGET"
+      rm -f $ORIGINAL
+    else
+      echo "Backing up $ORIGINAL to $BACKUP_TARGET"
+      install -D "$TARGET" "$BACKUP_TARGET" >/dev/null 2>&1
+    fi
+  done
+
+  stow --verbose --target=$HOME --adopt $STOW_PKG
+
+  # use stow to mirror the config directory
+}
+
+main() {
+  if [ "$ONLY_CONFIGS" == false ]; then
+    setup_os
+  else
+    echo "Only setting up config, skipping OS installations"
+  fi
+  setup_config
+}
+
+main
