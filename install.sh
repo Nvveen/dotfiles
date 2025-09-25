ddbb626c318badbd4abf886a4744a2cd66ef318c
Neal van Veen <nealvanveen@gmail.com>
2025-09-25 14:50:53 +0200

    move to stow

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ fi
 
 setup_omarchy() {
   # install packages
-  yay -S --noconfirm zsh firefox
+  yay -S --noconfirm zsh firefox stow
   chsh -s /usr/bin/zsh
 
   # Remove packages
@@ -32,26 +32,8 @@ setup_omarchy() {
 
   # oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  ln -svf $CWD/zsh/zshrc $HOME/.zshrc
-  ln -svf $CWD/zsh/zprofile $HOME/.zprofile
-  ln -svf $CWD/oh-my-zsh/aliases.sh $ZSH_CUSTOM/aliases.sh
 
-  # starship theme
-  mv -v $HOME/.config/starship.toml $HOME/.config/starship.toml_bak
-  starship preset tokyo-night -o $HOME/.config/starship.toml
-
-  # link plugins
-  plugins_dir="$CWD/oh-my-zsh/custom/plugins"
-  ls $plugins_dir | while read -r line; do
-    ln -svf "$plugins_dir/$line" "$HOME/.oh-my-zsh/custom/plugins/$line"
-  done
-
-  # nvim
-  ln -svf $CWD/nvim/lua/plugins/disabled.lua $HOME/.config/nvim/lua/plugins/disabled.lua
-  ln -svf $CWD/nvim/lua/plugins/cmp.lua $HOME/.config/nvim/lua/plugins/cmp.lua
-
-  # hyprland
-  ln -svf $CWD/hyprland/hyprland.conf $HOME/.config/hyprland/hyprland.conf
+  # mirror repo
 
   # theming
   omarchy-theme-set osaka-jade
