65a538fd2e4c1d5b2d00676421118960a1dc9f96
Neal van Veen <nealvanveen@gmail.com>
2025-10-01 13:56:25 +0000

    simplify from_git script

--- from_git.sh
+++ from_git.sh
@@ -11,35 +11,9 @@ main() {
   ssh -T git@github.com >/dev/null 2>&1
   ssh_exit_code=$?
 
-  if [[ $ssh_exit_code == 1 ]]; then
-    echo "GitHub authentication successful"
-  else
-    if [[ -f "$HOME/.ssh/id_rsa" ]]; then
-      echo "Please add your SSH key to GitHub:"
-      echo ""
-      cat $HOME/.ssh/id_rsa.pub
-      echo ""
-      read -p "Press [Enter] after adding the key to GitHub" </dev/tty
-    else
-      echo "Creating a new SSH key for GitHub..."
-      ssh-keygen -t rsa -b 4096 -C "nealvanveen@gmail.com" -f $HOME/.ssh/id_rsa -N ""
-      echo "Please add your SSH key to GitHub:"
-      echo ""
-      cat $HOME/.ssh/id_rsa.pub
-      echo ""
-      read -p "Press [Enter] after adding the key to GitHub" </dev/tty
-    fi
-
-    eval "$(ssh-agent -s)"
-    ssh-add $HOME/.ssh/id_rsa
-    echo "SSH key added to ssh-agent"
-
-    ssh -T git@github.com >/dev/null 2>&1
-    ssh_exit_code=$?
-    if [[ $ssh_exit_code != 1 ]]; then
-      echo "GitHub authentication failed. Exiting."
-      exit 1
-    fi
+  if [[ $ssh_exit_code != 1 ]]; then
+    echo "GitHub authentication failed"
+    exit 1
   fi
 
   git clone -b v2 --recurse-submodules git@github.com:Nvveen/dotfiles.git
