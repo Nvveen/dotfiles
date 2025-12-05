71fa0c1ea7e224a18f452d5fd9ab1d0c21328a7c
Neal van Veen <nealvanveen@gmail.com>
2025-12-05 14:32:07 +0100

    fix codespace

--- environments/codespace/setup.sh
+++ environments/codespace/setup.sh
@@ -4,7 +4,10 @@ setup_codespace() {
     sudo apt update && sudo apt install -y \
         vim \
         starship \
-        stow
+        stow \
+        zsh
+    sudo chsh $(whoami) -s $(which zsh)
+    mkdir -p ~/.ssh
     log "Setting up codespace environment"
 }
 
