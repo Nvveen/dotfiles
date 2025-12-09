255e0459c37ec6335d231c20bda89ba166d69210
Neal van Veen <nealvanveen@gmail.com>
2025-12-09 13:15:30 +0100

    make sure universe is enabled in codespaces

--- environments/codespace/setup.sh
+++ environments/codespace/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 setup_codespace() {
+    # make sure ubuntu universe is enabled
+    sudo add-apt-repository universe -y
     sudo apt update && sudo apt install -y \
         vim \
         starship \
